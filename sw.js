22d355ba3629750cb030a8cd9b50b012e9464ecb
Tomislav Jakic <mastermixxsb@gmail.com>
2026-09-21 17:59:24 +0200

    Bump SW ferm-v4.24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ferm-v4.23';
+const CACHE = 'ferm-v4.24';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 // Install — predcachiramo asete
